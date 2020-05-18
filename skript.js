1832e133a830831daf4f0dc88fb12165d9fada0c
di-monaco <59878303+di-monaco@users.noreply.github.com>
2020-05-18 15:26:00 +0200

    ff

---
+++ skript.js
@@ -0,0 +1,16 @@
+const options = {
+    bottom: '50px', 
+    right: '30px', 
+    left: 'unset',
+    time: '0.3s', 
+    mixColor: '#fff', 
+    backgroundColor: '#fff',  
+    buttonColorDark: '#C8102E',  
+    buttonColorLight: '#fff', 
+    saveInCookies: true, 
+    label: '🌓', 
+    autoMatchOsTheme: true 
+  }
+
+  const darkmode = new Darkmode(options);
+darkmode.showWidget();
